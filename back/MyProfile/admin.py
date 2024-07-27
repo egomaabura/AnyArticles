9f7feff3bd90cf51b56be3fdb2f45cbae779b0bc
egomaabura <k1222ars@gmail.com>
2024-07-27 22:07:12 +0900

    マイグレーション追加、管理サイトの設定追加

--- back/MyProfile/admin.py
+++ back/MyProfile/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "mail",
+        "user_id",
+        "deleted_at",
+    )
+    search_fields = (
+        "name",
+        "mail",
+        "user_id",
+    )
+    list_per_page = 30
+    ordering = (
+        "created_at",
+        "updated_at"
+    )
+    exclude = (
+        "user_id",
+        "created_at",
+        "updated_at",
+    )
+
+admin.site.register(User, UserAdmin)
